736419319d1eaaf47d8eb74225358eef3a2aff44
Lenovo T480 <hammadalii@outlook.com>
2023-03-17 14:21:47 +0500

    message successfully send to backend

--- src/app/stripe/stripe.component.ts
+++ src/app/stripe/stripe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { async } from '@angular/core/testing';
 import{ loadStripe, Stripe } from '@stripe/stripe-js'
+import { CheckoutService } from './../checkout.service';
 
 @Component({
   selector: 'app-stripe',
@@ -12,15 +13,17 @@ export class StripeComponent implements OnInit {
 title = 'stripe-payment';
 personName:string = ''
 address:string = ''
+success: boolean = false
+failure:boolean = false
 
 private stripe: Stripe;
 
-constructor() {
+constructor(private checkout: CheckoutService) {
 
 }
 
 async ngOnInit() {
-  this.stripe = await loadStripe('')
+  this.stripe = await loadStripe('pk_test_51MllwGBUJKiCM3GCrQYp5SLrIFAd34f1dhnhG4osqY3D3UxvX5drgEgPknaJlgKVFXRwjoFkcTVBNY4RlM8CneAG00Vi5JTpLO')
 
   const elements = this.stripe.elements();
   const card = elements.create('card');
@@ -39,9 +42,22 @@ async ngOnInit() {
         currency: 'mxn'
       };
 
+      const paymentstripe = (stripeToken: any) => {
+        this.checkout.makePayment(stripeToken).subscribe((data: any) => {
+          console.log(data);
+          if (data.data === "success") {
+            this.success = true
+          }
+          else {
+            this.failure = true
+          }
+      });
+    }
+
       try{
         const result = await this.stripe.createSource(card, ownerInfo);
         console.log('result:', result)
+        paymentstripe(result.source)
       }
       catch(e) {
         console.warn(e.message)
